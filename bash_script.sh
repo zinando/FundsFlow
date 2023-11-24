ed62ea788001b1707b1122faff191b0ab609c5b8
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2023-11-24 03:35:46 +0100

    Create bash_script.sh that contains commands to run when deploying on render.com

---
+++ bash_script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Script to execute when deploying on render.com
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+pip install Flask
+pip install sqlalchemy
+pip install gunicorn
+pip install flask_sqlalchemy
