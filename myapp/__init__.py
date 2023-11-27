bd3d993e9d05b2c0c6c97aa7bd2de248d93c2b3f
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2023-11-27 14:10:25 +0100

    update cors config

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
-#from flask_cors import CORS
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
-#cors = CORS(app, resources={r"/*": {"origins": "*"}})
-#app.config['CORS_HEADERS'] = "Content-Type"
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = "Content-Type"
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
